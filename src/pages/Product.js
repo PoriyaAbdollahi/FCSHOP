597e8eda40345a09f6f3b551b25029515bad3e00
PoriyaAbdollahi <poriyaa58@gmail.com>
2021-08-06 18:59:57 +0430

    improving product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,11 +1,45 @@
 import React from 'react'
+import { Row , Col , Image ,ListGroup , Button} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import products from '../products'
+const Product = ({match}) => {
 
-const Product = () => {
+const product =    products.find((item)=>{
+      return item._id === match.params.id
+   })
    return (
       <div>
-        Products 
+       <Link to="/" className="btn btn-light my-3">لینک بازگشت به صفحه اصلی</Link>
+       
+       <Row>
+          <Col md={6}>
+             <Image src={product.image} fluid/>
+          </Col>
+         
+          <Col md={3}>
+          <ListGroup variant="flush">
+          <ListGroup.Item>
+                <h3>{product.name}</h3>
+             </ListGroup.Item>
+          <ListGroup.Item>
+                <h3>{product.price}</h3>
+             </ListGroup.Item>
+             <ListGroup.Item>
+                <h3>{product.description}</h3>
+             </ListGroup.Item>
+          </ListGroup>
+          </Col>
+          <Col md={3}>
+             <ListGroup variant="flush">
+                <ListGroup.Item>
+                   <Button className="btn-block" type="button"> افزودن به سبد خرید</Button>
+                </ListGroup.Item>
+             </ListGroup>
+          </Col>
+       </Row>
       </div>
    )
+   
 }
 
 export default Product
