6901ec56c581f097afb6614cdc57e4fe8688c182
PoriyaAbdollahi <poriyaa58@gmail.com>
2021-08-10 11:15:04 +0430

    getting products from backend using axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../Components/Product/Product'
-import products from '../products'
-
+// import products from '../products'
+import axios from 'axios'
 const Home = () => {
+const [products,setProducts] = useState([]) 
+useEffect(() => {
+ const sendRequest =async () =>  {
+    const response = await axios.get("http://localhost:8000/api/products")
+    setProducts(response.data)
+   }
+   sendRequest()
+  
+}, [])
    return (
       <div>
       <h1>محصولات</h1> 
