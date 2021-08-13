9e4d25ede99875de2e84f129cf8d34b18ca7545f
Poriya Abdollahi <72283109+PoriyaAbdollahi@users.noreply.github.com>
2021-08-13 08:55:41 +0430

    backend basic

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const productRoutes = require('./routes/product-routes')
+
+const app = express()
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  next()
+})
+
+app.use('/api', productRoutes)
+
+app.listen(8000)
