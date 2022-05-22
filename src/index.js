84a4612f8cc410fffb8e0899198b0b3c1ea8809b
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-05-22 18:06:54 +0430

    first text deploy

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './index.css'
-import React from 'react'
-import reactDom from 'react-dom'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css'
-import {Provider} from 'react-redux'
-import store from './store'
-reactDom.render( <Provider store = {store}><App/></Provider>, document.getElementById('root') )
