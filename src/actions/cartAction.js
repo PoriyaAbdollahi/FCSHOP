d8ce943c209b4e0b86fde0bf3fbf21650106aa72
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-05-22 17:52:33 +0430

    make website suitable for github pages

--- src/actions/cartAction.js
+++ src/actions/cartAction.js
@@ -1,15 +1,17 @@
 import axios from "axios";
-
+import products from "../products";
 export const addTocart = (id) => async(dispatch,getState) => {
-   const { data } = await axios.get(`http://localhost:8000/api/products/${id}`)
-
+  // const { data } = await axios.get(`http://localhost:8000/api/products/${id}`)
+  let product = products.find((product)=>{
+   return product._id == id
+})
    dispatch({
       type : 'CART_ADD_ITEM',
       payload : {
-         product: data._id,
-         name:data.name,
-         image:data.image,
-         price:data.price,
+         product: product._id,
+         name:product.name,
+         image:product.image,
+         price:product.price,
       }
    })
    localStorage.setItem('cartItems',JSON.stringify(getState().cart.cartItems))
